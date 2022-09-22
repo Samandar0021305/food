0c2b31f6b5ed3f0d826d9dd489b0dc7ba7cfdf04
Samandar0021305 <samandar021325@gmail.com>
2022-09-22 09:32:47 +0500

    init

--- src/components/home-components/news/News.jsx
+++ src/components/home-components/news/News.jsx
@@ -1,6 +1,24 @@
+import "./news.scss";
+import api from "./NewsApi.js";
+
 function News (){
     return(
-        <div className="news"></div>
+        <div className="news">
+            <div className="container">
+                <ul className="news__list">
+                    {api.map((e)=>{
+                        return(
+                            <li key={e.id}>
+                                <img src={e.img} alt="logo" />
+                                <h2>{e.title}</h2>
+                                <p>{e.text}</p>
+                                <button className="news__btn"> More details <span></span>    </button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/home-components/news/NewsApi.js
@@ -0,0 +1,11 @@
+import news1 from "../../../assets/img/news1.jpg";
+import news2 from "../../../assets/img/news2.jpg";
+import news3 from "../../../assets/img/news3.jpg";
+
+const api = [
+    {id:1 , img:news1 , title:"The great advantage of the sales office" , text :"Dear friends!If you visit Yangi.."},
+    {id:2 , img:news2 , title:"Automatic parking system" , text :"Hi people! Yangi Qo’yliq Bozori.."},
+    {id:3 , img:news3 , title:"The great advantage of the sales office" , text :"Dear friends!If you visit Yangi.."}
+]
+
+export default api;
