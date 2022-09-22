cd695af23abf6a7d2ccde4f5c224f87df133619d
Samandar0021305 <samandar021325@gmail.com>
2022-09-22 09:05:57 +0500

    init

---
+++ src/components/home-components/news/News.jsx
@@ -0,0 +1,7 @@
+function News (){
+    return(
+        <div className="news"></div>
+    )
+}
+
+export default News;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -2,6 +2,7 @@ import AboutSection from "../../components/home-components/about-section/About-s
 import Header from "../../components/home-components/header/Header";
 import Section from "../../components/home-components/section/Section";
 import Topbar from "../../components/topbar/Topbar";
+import News from "../../components/home-components/news/News";
 
 function Home(){
     return(
@@ -10,6 +11,7 @@ function Home(){
             <Header />
             <Section />
             <AboutSection />
+            <News />
         </div>
     )
 }
