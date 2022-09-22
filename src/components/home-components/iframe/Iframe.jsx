87d905716e85b7b6638b8d3fc1abb7ab3e5017b2
Samandar0021305 <samandar021325@gmail.com>
2022-09-22 09:46:48 +0500

    init

---
+++ src/components/home-components/iframe/Iframe.jsx
@@ -0,0 +1,18 @@
+import "./iframe.scss";
+// import Iframe1 from 'react-iframe'
+function Iframe(){
+    return(
+        <div className="iframe container">
+            <ul className="iframe__list">
+                <li className="iframe__item">
+                    {/* <Iframe1 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3002.0223522372903!2d69.32403422018638!3d41.19948625903398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1663821791752!5m2!1sru!2s"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></Iframe1> */}
+                </li>
+                <li className="iframe__item">
+
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Iframe;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,8 +1,11 @@
+// components
 import AboutSection from "../../components/home-components/about-section/About-section";
 import Header from "../../components/home-components/header/Header";
 import Section from "../../components/home-components/section/Section";
 import Topbar from "../../components/topbar/Topbar";
 import News from "../../components/home-components/news/News";
+import Iframe from "../../components/home-components/iframe/Iframe";
+
 
 function Home(){
     return(
@@ -12,6 +15,7 @@ function Home(){
             <Section />
             <AboutSection />
             <News />
+            <Iframe />
         </div>
     )
 }
