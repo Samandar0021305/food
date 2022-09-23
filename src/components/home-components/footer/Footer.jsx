f66cf5bcd873776d7341630091daabecba4ddf5a
Samandar0021305 <samandar021325@gmail.com>
2022-09-23 09:09:28 +0500

    init

--- src/components/home-components/footer/Footer.jsx
+++ src/components/home-components/footer/Footer.jsx
@@ -1,13 +1,21 @@
 import "./footer.scss";
 
+import img from "../../../assets/img/logo-footer.svg";
+
 function Footer(){
     return(
         <footer className="footer">
             <div className="container">
                 <div className="footer__topbar">
                     <ul className="footer__topbar__list">
-                        <li></li>
+                        <li className="footer__topbar__item">
+                            <img src={img} alt="logo" />
+                        </li>
+                        <li className="footer__topbar__item">
+                            <h2>“Yangi Qo’yliq Dehqon Bozori”</h2>
+                        </li>
                     </ul>
+                    
                 </div>
             </div>
         </footer>
