7ca10bfb9b705e4b1babbb207c58a4b0e4d7ddbf
Samandar0021305 <samandar021325@gmail.com>
2022-09-23 09:49:33 +0500

    init

---
+++ src/components/about-components/section/Section-about.jsx
@@ -0,0 +1,13 @@
+import "./section-about.scss";
+
+
+function SectionAbout(){
+    return(
+        <section className="sectionAbout">
+            <div className="container"></div>
+        </section>
+    )
+}
+
+
+export default SectionAbout;
--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -1,11 +1,13 @@
 import Topbar from "../../components/topbar/Topbar"
 import Header from "../../components/about-components/header/Header.jsx";
+import SectionAbout from "../../components/about-components/section/Section-about";
 
 function About(){
     return(
         <>
          <Topbar />
          <Header />
+         <SectionAbout />
         </>
     )
 }
