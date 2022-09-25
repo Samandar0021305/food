43be019397e9dde80dc5d66ac800bbd0406f6997
Samandar0021305 <samandar021325@gmail.com>
2022-09-25 11:05:20 +0500

    init

--- src/components/about-components/aboutUs/AboutUs.jsx
+++ src/components/about-components/aboutUs/AboutUs.jsx
@@ -1,6 +1,10 @@
 import "./aboutUs.scss";
-
+// img
 import aboutUsImg from "../../../assets/img/aboutUs.jpg";
+import aboutUS1 from "../../../assets/img/aboutUs1.jpg";
+import aboutUS2 from "../../../assets/img/aboutUs2.jpg";
+import aboutUS3 from "../../../assets/img/aboutUs3.jpg";
+import aboutUS4 from "../../../assets/img/aboutUs4.jpg";
 
 function AboutUs(){
     return(
@@ -14,6 +18,53 @@ function AboutUs(){
                     <p className="AboutUs__text">FOODCITY is the first agricultural cluster in Uzbekistan and the most modernized trading platform. For the first time in Uzbekistan, the Cross-Docking logistics operation system was applied, this system excludes the stage of storing goods in a warehouse, in simple terms you buy a product from the place of its assembly, avoiding a bunch of warehouses and resellers, thereby you get a fresh product directly from the place of its germination, and in this area there are 5 cross-docks with a different range of products.</p>
                   </li>
             </ul>
+
+
+            <ul className="AboutUs__box__list">
+                <li className="AboutUs__box__item">
+                    <img src={aboutUS1} alt="" />
+                    <h2 className="AboutUs__box__title">
+                    CROSS DOC
+                    </h2>
+                    <p className="AboutUs__box__text">
+                    5 cross docks Transfer of..
+                    </p>
+                    <button className="AboutUs__box__btn">Подробнее</button>
+                </li>
+                <li className="AboutUs__box__item">
+                    <img src={aboutUS1} alt="" />
+                    <h2 className="AboutUs__box__title">
+                    CROSS DOC
+                    </h2>
+                    <p className="AboutUs__box__text">
+                    5 cross docks Transfer of..
+                    </p>
+                    <button className="AboutUs__box__btn">Подробнее</button>
+                </li>
+                <li className="AboutUs__box__item">
+                    <img src={aboutUS1} alt="" />
+                    <h2 className="AboutUs__box__title">
+                    CROSS DOC
+                    </h2>
+                    <p className="AboutUs__box__text">
+                    5 cross docks Transfer of..
+                    </p>
+                    <button className="AboutUs__box__btn">Подробнее</button>
+                </li>
+                <li className="AboutUs__box__item">
+                    <img src={aboutUS1} alt="" />
+                    <h2 className="AboutUs__box__title">
+                    CROSS DOC
+                    </h2>
+                    <p className="AboutUs__box__text">
+                    5 cross docks Transfer of..
+                    </p>
+                    <div className="AboutUs__box__box">
+                    <button className="AboutUs__box__btn">Подробнее</button>
+                    </div>
+                </li>
+            </ul>
+
         </div>
     )
 }
