efc0634faf3d20171bb56f3c9762a20b81f26457
Samandar0021305 <samandar021325@gmail.com>
2022-09-27 09:52:04 +0500

    init

---
+++ src/components/warehouses/warehousesSection/WarehousesSection.jsx
@@ -0,0 +1,28 @@
+import "./WarehousesSection.scss";
+
+import WarehousesSection1 from "../../../assets/img/WarehousesSection1.jpeg"
+
+function WarehousesSection(){
+    return(
+        <div className="WarehousesSection container">
+                <ul className="WarehousesSection__list__one">
+                    <li className="WarehousesSection__list__item">
+                        <img src={WarehousesSection1} alt="" />
+                    </li>
+                    <li className="WarehousesSection__list__item">
+                        <h2>Storage services</h2>
+                        <p>OOD CITY provides storage and rental services for small cells, which helps to minimize logistical
+                            our client’s costs.
+                            Using modern video surveillance systems, we guard the warehouse around the clock. We are for our clients
+                            we provide pallets, we carry out loading and unloading of goods in any type of transport, when
+                            if necessary, we use WMS (Warehouse Management System) and
+                            We provide daily reporting on the movement, balances, defects and other parameters of goods. For
+                            more reliability, we regularly conduct product inventories</p>
+                    </li>
+                </ul>
+                <span className="WarehousesSection__bg"></span>
+        </div>
+    )
+}
+
+export default WarehousesSection;
--- src/page/warehouses/Warehouses.jsx
+++ src/page/warehouses/Warehouses.jsx
@@ -1,12 +1,14 @@
 import Topbar from "../../components/topbar/Topbar";
 
 import WarehousesHader from "../../components/warehouses/warehousesHeader/WarehousesHeader.jsx";
+import WarehousesSection from "../../components/warehouses/warehousesSection/WarehousesSection";
 
 function Warehouses(){
     return(
         <>
          <Topbar /> 
          <WarehousesHader />
+         <WarehousesSection />
         </>
     )
 }
