85e4eed01685af9c39db8886ba4c9a8d802ca188
Samandar0021305 <samandar021325@gmail.com>
2022-09-27 09:38:18 +0500

    init

---
+++ src/components/warehouses/warehousesHeader/WarehousesHeader.jsx
@@ -0,0 +1,13 @@
+import "./warehousesHeader.scss";
+
+function WarehousesHader(){
+    return(
+        <div className="warehousesHeader">
+             <div className="container warehousesHeader__box">
+                <h2>Safe storage in Tashkent</h2>
+             </div>
+        </div>
+    )
+}
+
+export default WarehousesHader;
--- src/page/warehouses/Warehouses.jsx
+++ src/page/warehouses/Warehouses.jsx
@@ -1,9 +1,12 @@
 import Topbar from "../../components/topbar/Topbar";
 
+import WarehousesHader from "../../components/warehouses/warehousesHeader/WarehousesHeader.jsx";
+
 function Warehouses(){
     return(
         <>
          <Topbar /> 
+         <WarehousesHader />
         </>
     )
 }
